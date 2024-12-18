22a62fe65d3eae9b69bc897e24f1e7a11e3d7a8e
Frostleaft07 <zx7unknow@gmail.com>
2024-12-18 11:59:08 +0700

    k

--- rom.sh
+++ rom.sh
@@ -3,7 +3,7 @@ sudo apt-get update -y && sudo apt-get install dialog bash sed wget git curl zip
 rm -rf .repo/local_manifests/
 find . -delete
 
-repo init -u https://github.com/Los-FE/manifest -b lineage-18.1 --depth=1
+repo init -u https://github.com/AospExtended/manifest -b 11.x --depth=1
 
 /opt/crave/resync.sh ; repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -20,8 +20,8 @@ rm -rf packages/apps/ManagedProvisioning
 git clone --depth=1 https://android.googlesource.com/platform/packages/apps/ManagedProvisioning -b android11-release packages/apps/ManagedProvisioning
 rm -rf packages/providers/DownloadProvider/
 git clone https://android.googlesource.com/platform/packages/providers/DownloadProvider -b android11-release packages/providers/DownloadProvider
-rm -rf packages/apps/Settings
-git clone https://github.com/Frostleaft07/android_packages_apps_Settings-fe -b lineage-18.1 packages/apps/Settings
+#rm -rf packages/apps/Settings
+#git clone https://github.com/Frostleaft07/android_packages_apps_Settings-fe -b lineage-18.1 packages/apps/Settings
 
 otsu=$(pwd)
 depis=device/realme/RMX2185
@@ -30,11 +30,11 @@ export BUILD_USERNAME=frost
 export BUILD_HOSTNAME=otsu-builder
 export TZ=Asia/Jakarta
 
-lunch lineage_RMX2185-user
+lunch lineage_RMX2185-userdebug
 make bacon
 
 cd out/target/product/RMX2185/
 
-curl bashupload.com -T LineageFE-v11.6*2185*.zip
+curl bashupload.com -T *2185*.zip
 
-curl bashupload.com -T LineageFE-v11.6*2185*.zip
+curl bashupload.com -T *2185*.zip
